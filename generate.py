641bf4e12a05b8049d1472818fc4dad969e858e1
Ameena Naveeda <ameena.naveeda01@gmail.com>
2025-05-25 23:10:45 +0400

    Chapter-6 Finetune

--- generate.py
+++ generate.py
@@ -35,3 +35,31 @@ def text_to_token_ids(text, tokenizer):
 def token_ids_to_text(token_ids, tokenizer):
     flat = token_ids.squeeze(0)
     return tokenizer.decode(flat.tolist())
+
+# idx is a (batch_size, n_tokens) array of indices in the current context
+def generate_text_sample(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        """
+        crops current context if it exceeds the supported context size
+        eg: if LLM supports only 5 tokens and the context size is 10
+        only 5 tokens are used as context
+        """
+        idx_cond = idx[:,-context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        """
+        Focuses only on the last time step so that (batch_size, n_tokens, vocab_size)
+        becomes (batch_size, vocab_size)
+        """
+        logits = logits[:,-1,:]
+        # probas has shape (batch_size, vocab_size)
+        probas = torch.softmax(logits, dim=-1)
+        # idx_next has shape (batch_size, 1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        """
+        Append sampled index tot he running sequence
+        where idx has shape (batch, n_tokens+1)
+        """
+        idx = torch.cat((idx, idx_next), dim=-1)
+    return idx
